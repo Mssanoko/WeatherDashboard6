a659bd4d1a9e3de52fa623714b551f107dec6a58
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-08 10:00:11 -0400

    js value of the input

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Value of the input
-var city = $("search-value").val();
+var city = $("#searchValue").val();
 
 //Api Key 
 const apiKey = "&appid=e5d49d791f7868e4c7703b8e068cc7f8";
 
 var date = new Date();
+
+//Value of the input from the user 
+city = $("searchValue").val();
